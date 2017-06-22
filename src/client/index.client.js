5b5b6c2d2f986bdcdefff92a70ffd6ebf48bba3a
Daniel Gonzalez Reina <dangonrei@gmail.com>
2017-06-22 16:38:32 -0400

    Connection to the WebSocket

--- src/client/index.client.js
+++ src/client/index.client.js
@@ -4,7 +4,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Layout from './components/Layout';
 
+// TODO server here is hardcoded, but it shuold come from the user or device
+const servers = ['ws://localhost:4000'];
+const wsConnections = {};
+
 window.onload = function () {
+  for (let server of servers) {
+    wsConnections[server] = new WebSocket(server);
+    wsConnections[server].onmessage = event => {
+      store.dispatch(event.data);
+    };
+  }
+
   ReactDom.render(
     <Layout/>,
     document.getElementById('root')
