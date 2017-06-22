247a23ec592bdb3d11565233b974d47976899030
Daniel Gonzalez Reina <dangonrei@gmail.com>
2017-06-22 16:45:46 -0400

    Added Redux to the stack

--- src/client/index.client.js
+++ src/client/index.client.js
@@ -2,11 +2,16 @@
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers/reducers';
 import Layout from './components/Layout';
 
 // TODO server here is hardcoded, but it shuold come from the user or device
 const servers = ['ws://localhost:4000'];
 const wsConnections = {};
+
+const store = createStore(reducers);
 
 window.onload = function () {
   for (let server of servers) {
@@ -17,7 +22,9 @@ window.onload = function () {
   }
 
   ReactDom.render(
-    <Layout/>,
+    <Provider store={store}>
+      <Layout/>
+    </Provider>,
     document.getElementById('root')
   );
 };
---
+++ src/client/reducers/reducers.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export default function reducers (state, action) {
+  return state;
+}
