e302ce46336f03362f946a6a2ba669369d164f07
Daniel Gonzalez Reina <dangonrei@gmail.com>
2017-07-05 15:47:19 -0400

    Now it handles group of messages sent from the server, as when it connects

--- src/client/reducers/messageReducer.js
+++ src/client/reducers/messageReducer.js
@@ -6,10 +6,20 @@ export default function messageReducer (state = [], action) {
     const newMessage = {
       htmlMessage: action.payload.htmlMessage,
       rawMessage: action.payload.rawMessage,
-      ownerId: action.payload.user
+      ownerId: action.payload.user,
+      timestamp: action.payload.timestamp
     };
     return [...state.splice(-100), newMessage];
     break;
+
+  case 'MESSAGE_GROUP':
+    const newMessageGroup = action.payload.map(v => ({
+      htmlMessage: v.htmlMessage,
+      rawMessage: v.rawMessage,
+      ownerId: v.user,
+      timestamp: v.timestamp
+    }));
+    return [...state.splice(-100), ...newMessageGroup];
 
   default:
     return state;
